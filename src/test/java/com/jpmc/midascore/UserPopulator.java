fb61f598698e454732138e5b2c3dfb0825eab2ec
adam brunson <adam.brunson@yahoo.com>
2025-08-16 11:42:51 -0400

    test one complete, (set up)

--- src/test/java/com/jpmc/midascore/UserPopulator.java
+++ src/test/java/com/jpmc/midascore/UserPopulator.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserPopulator {
+public class  UserPopulator {
     @Autowired
     private FileLoader fileLoader;
 
